29b7a158aa02cf0f62b32f7de698c7ae70d4cecf
splo2t <yu990410@naver.com>
2021-02-14 23:54:26 +0900

    update_h

--- app.py
+++ app.py
@@ -6,14 +6,13 @@ import socket
 from flask import Flask, render_template, request, session, redirect, url_for
 app = Flask(__name__, static_url_path='/static')
 
-
-@app.route('/', methods=['GET', 'POST'])
-def missionComplete():
+name = '홍길동'
+password = '1234'
+title = '기본 제목'
+content = '기본 본문'
+@app.route('/sending', methods=['GET', 'POST'])
+def sending():
     if request.method == 'POST':
-        name = request.form['name']
-        title = request.form['title']
-        password = request.form['password']
-        content = request.form['content']
         options = webdriver.ChromeOptions()
         options.add_argument('--headless')
         options.add_argument('--no-sandbox')
@@ -36,27 +35,19 @@ def missionComplete():
         driver.find_element_by_css_selector('#birthDay').send_keys('07')
         driver.find_element_by_css_selector('#btnNext').click()
 
-
         # 팝업에서 훈련병 선택
         driver.switch_to_window(driver.window_handles[1])
         driver.find_element_by_css_selector('.choice').click()
-
-
 
         # 편지쓰기
         driver.switch_to_window(driver.window_handles[0])
         driver.find_element_by_css_selector('#btnNext').click()
 
-
         # 인터넷편지 작성
         driver.find_element_by_xpath("//div[@class='UIbtn']/span[@class='wizBtn large Ngray normal btnR']").click()
 
-
-
         # 우편번호 및 주소(디폴트 유현욱 자취방)
         driver.find_element_by_css_selector('#senderZipcode').click()
-
-
 
         driver.switch_to_window(driver.window_handles[1])
 
@@ -65,8 +56,6 @@ def missionComplete():
         driver.find_element_by_xpath("/html/body/form[2]/div/div/div[1]/div[1]/fieldset/span/input[2]").click()  # 검색 버튼
         driver.find_element_by_xpath(
             "/html/body/form[2]/div/div/div[1]/div[2]/table/tbody/tr[1]/td[2]/a/div/div").click()  # 첫번째 목록 선택 목록 여러개면 div[]로 선택 가능할듯
-
-
 
         driver.find_element_by_css_selector('#rtAddrDetail').send_keys("101호")
         driver.find_element_by_css_selector('.btn-bl').click()
@@ -78,15 +67,28 @@ def missionComplete():
         driver.find_element_by_css_selector('#relationship').send_keys("친구의 친구")  # 관계
         driver.find_element_by_css_selector('#title').send_keys(title)  # 제목
         driver.find_element_by_css_selector('#contents').send_keys(content)  # 내용 #1200자
-        driver.find_element_by_css_selector('#password').send_keys(password)  # 비밀번호 #비밀번호 다르게 해야될까? 아니면 master key마냥 하나로 쭉 가도 안전할까?
+        driver.find_element_by_css_selector('#password').send_keys(
+            password)  # 비밀번호 #비밀번호 다르게 해야될까? 아니면 master key마냥 하나로 쭉 가도 안전할까?
         try:
             driver.find_element_by_css_selector('.submit').click()  # 작성 완료
             return '<h1>success</h1>'
         except Exception as error:
             return '<h1>ERROR... Pleas retry</h1>'
         # 근데 이게 가끔 작성 완료가 안될 때가 있거든? 그걸 어떻게 예외 처리해야할지 고민중...
-        #driver.find_element_by_xpath(
+        # driver.find_element_by_xpath(
         #    "/html/body/div[1]/div[2]/div/div[2]/div/div/div/div/div[2]/span/input").click()  # 목록으로
+    else:
+        return render_template('main.html')
+
+@app.route('/', methods=['GET', 'POST'])
+def missionComplete():
+    if request.method == 'POST':
+        name = request.form['name']
+        title = request.form['title']
+        password = request.form['password']
+        content = request.form['content']
+        return render_template('test.html')
+
 
     else:
         return render_template('main.html')
