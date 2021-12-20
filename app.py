b48fd1219357e4dbe7336b918148e2ed7cbb087d
yymin1022 <yymin1022@gmail.com>
2021-12-20 13:36:02 +0900

    Update Port

--- app.py
+++ app.py
@@ -106,6 +106,6 @@ def missionComplete():
 if __name__ == '__main__':
     IP = str(socket.gethostbyname(socket.gethostname()))
     print(IP)
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=8080, debug=False)
     app.run()
 
